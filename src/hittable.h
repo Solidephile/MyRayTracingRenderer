cb56de616dd7abdc2080f06ae2c40eebe5d7be64
Solidephile <cocoxu2021@126.com>
2024-10-03 22:23:21 +0800

    deleted:    build/release/main.exe new file:   images/1.ppm new file:   images/2.ppm new file:   images/earthmap.jpg new file:   src/aabb.h new file:   src/bvh.h modified:   src/camera.h new file:   src/external/stb_image.h modified:   src/hittable.h modified:   src/hittable_list.h modified:   src/interval.h modified:   src/main.cpp modified:   src/material.h new file:   src/perlin.h modified:   src/ray.h new file:   src/rtw_stb_image.h modified:   src/rtweekend.h modified:   src/sphere.h new file:   src/texture.h

--- src/hittable.h
+++ src/hittable.h
@@ -2,6 +2,7 @@
 #define HITTABLE_H
 
 #include "rtweekend.h"
+#include "aabb.h"
 
 class material;
 
@@ -12,6 +13,8 @@ public:
     vec3 normal;
     shared_ptr<material> mat;
     double t;
+    double u;
+    double v;
     bool front_face;
 
     void set_face_normal(const ray &r, const vec3 &outward_normal)
@@ -29,6 +32,7 @@ class hittable
 public:
     virtual ~hittable() = default;
     virtual bool hit(const ray &r, interval ray_t, hit_record &rec) const = 0;
+    virtual aabb bounding_box() const = 0;
 };
 
 #endif
